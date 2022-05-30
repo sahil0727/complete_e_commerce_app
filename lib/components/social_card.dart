7786aba11650a2175eaf8ba9dbe49ed9a072279d
kachariya <sahiltechnism@gmail.com>
2022-05-29 23:46:00 -0500

    login screen

---
+++ lib/components/social_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../size_config.dart';
+class SocialCard extends StatelessWidget {
+  final String icon;
+  final Function press;
+
+  const SocialCard({
+    Key? key,
+    required this.icon,
+    required this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: ()=>press(),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(10),),
+        padding: EdgeInsets.all(
+          getProportionateScreenWidth(12),
+        ),
+        width: getProportionateScreenWidth(40),
+        height: getProportionateScreenHeight(40),
+        decoration: BoxDecoration(
+          color: const Color(0xfff5f6f9),
+          border: Border.all(
+            color: Colors.black26,
+            width: 0.75,
+          ),
+          shape: BoxShape.circle,
+        ),
+        child: SvgPicture.asset(icon),
+      ),
+    );
+  }
+}
--- lib/routs.dart
+++ lib/routs.dart
@@ -1,3 +1,6 @@
+
+import 'package:complete_e_commerce_app/screens/forget_password/forget_password_screen.dart';
+import 'package:complete_e_commerce_app/screens/login_success/login_success_screen.dart';
 import 'package:complete_e_commerce_app/screens/sign_in/sign_in_screen.dart';
 import 'package:complete_e_commerce_app/screens/splash/splash_screen.dart';
 import 'package:flutter/widgets.dart';
@@ -8,4 +11,6 @@ import 'package:flutter/widgets.dart';
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => const SplashScreen(),
   SignInScreen.routeName: (context) => const SignInScreen(),
+  ForgetPasswordScreen.routeName :(context)=>const ForgetPasswordScreen(),
+  LoginSuccessScreen.routeName:(context)=>const LoginSuccessScreen(),
 };
